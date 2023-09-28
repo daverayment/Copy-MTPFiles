3d379d331263220d57af390d1990e2d731af3e83
Dave Rayment <dave.rayment@gmail.com>
2023-09-28 17:25:26 +0100

    Mocks fixed and now path-independent

--- Tests/Mocks.ps1
+++ Tests/Mocks.ps1
@@ -1,82 +1,142 @@
-class MockFileSystem {
-    [bool] $IsCaseSensitive
-    [string[]] $Entries
-    [string] $Name
+. $PSScriptRoot\..\src\Copy-MTPFilesSharedFunctions.ps1
 
-    MockFileSystem([string]$Name, [bool]$IsCaseSensitive, [string[]]$Entries) {
+class MockFileSystem {
+    [bool]$IsHostFileSystem
+    [string[]]$Entries
+    [string]$Name
+    [string]$BasePath
+
+    MockFileSystem([string]$Name, [bool]$IsHostFileSystem, [string[]]$Entries, [string]$BasePath) {
         $this.Name = $Name
-        $this.IsCaseSensitive = $IsCaseSensitive
-        $this.Entries = $entries
+        $this.IsHostFileSystem = $IsHostFileSystem
+        $this.Entries = $Entries
+        $this.BasePath = $BasePath
     }
 
-    [void] AddItem([string] $Path) {
+    [void] AddItem([string]$Path) {
         $this.Entries += $Path
     }
 
-    [void] RemoveItem([string] $Path, [string] $PathType) {
+    [void] RemoveItem([string]$Path, [string]$PathType) {
         if ($PathType -eq 'Leaf') {
             $Path += '!'
         }
 
-        if ($this.IsCaseSensitive) {
-            # Remove is case-sensitive by default.
-            $this.Entries.Remove($Path)
-        } else {
+        if ($this.IsHostFileSystem) {
             $item = $this.Entries | Where-Object { $_ -ieq $Path }
             if ($item) {
                 $this.Entries.Remove($item)
             }
+        } else {
+            # Remove is case-sensitive by default.
+            $this.Entries.Remove($Path)
         }
     }
 
-    [bool] Contains([string] $Path, [string] $PathType) {
+    [bool] Contains([string]$Path, [string]$PathType) {
+        $Path = $this.NormalisePath($Path)
+
         if ($PathType -eq 'Leaf') {
             $Path += '!'
+        } elseif (-not $PathType) {
+            return $this.Contains($Path, 'Container') -or
+                $this.Contains($Path, 'Leaf')
         }
 
-        if ($this.IsCaseSensitive) {
-            return $this.Entries -ccontains $Path
+        if ($Path -eq '' -and $PathType -ne 'Leaf') {
+            # The path is the base path, so it's a valid container path.
+            return $true
+        }
+
+        $hasWildcard = $Path -match '[*?]'
+
+        if ($this.IsHostFileSystem) {
+            if ($hasWildcard) {
+                # Use -like for wildcard matching.
+                return $this.Entries -like $Path
+            } else {
+                return $this.Entries -contains $Path
+            }
         } else {
-            return $this.Entries -contains $Path
+            if ($hasWildcard) {
+                $regex = Convert-WildcardsToRegex -Patterns @($Path)
+                return [bool]($this.Entries | Where-Object { $_ -match $regex })
+            }
+            return $this.Entries -ccontains $Path
         }
     }
 
-    [Object] GetEntry([string] $Path) {
-        return $this.GetEntry($Path, 'Container', $false)
+    # Ensure paths are formatted properly for the mock host filesystem.
+    [string] NormalisePath([string]$Path) {
+        # Remove the script root from the path, turning it into a relative path.
+        $Path = $Path.Replace($this.BasePath, '')
+
+        # Normalise slashes and remove trailing slash(es).
+        $Path = $Path.Replace('\', '/').TrimEnd('/')
+
+        # Ensure relative paths start with "./"
+        if ($this.IsHostFileSystem -and
+            (-not [IO.Path]::IsPathRooted($Path)) -and 
+            (-not $Path.StartsWith('./'))) {
+            $Path = "./$Path"
+        }
+
+        return $Path
     }
 
-    [Object] GetEntry([string] $Path, [string] $PathType) {
+    [Object] GetEntry([string]$Path) {
+        return $this.GetEntry($Path, 'Any', $false)
+    }
+
+    [Object] GetEntry([string]$Path, [string]$PathType) {
         return $this.GetEntry($Path, $PathType, $false)
     }
 
-    [Object] GetEntry([string] $Path, [string] $PathType, [bool] $CreateIfNotExists) {
+    [Object] GetEntry([string]$Path, [string]$PathType, [bool]$CreateIfNotExists) {
+        if ($PathType -eq 'Any') {
+            $PathType = if ($this.Contains($Path, 'Leaf')) { 'Leaf' } else { 'Container' }
+        }
+
+        $Path = $this.NormalisePath($Path)
+        $isFound = $this.Contains($Path, $PathType)
+
         if ($PathType -eq 'Leaf') {
             $Path += '!'
         }
 
-        $isFound = $this.Contains($Path, $PathType)
-
         if ($isFound) {
-            $item = if ($this.IsCaseSensitive) {
-                $this.Entries | Where-Object { $_ -ceq $Path }
-            } else {
+            # NB: device paths are case-sensitive by default.
+            $item = if ($this.IsHostFileSystem) {
                 $this.Entries | Where-Object { $_ -eq $Path }
+            } else {
+                $this.Entries | Where-Object { $_ -ceq $Path }
+            }
+
+            $itemName = switch ($Path) {
+                '/' { '' }
+                '' { '' }
+                default { Split-Path -Path $Path -Leaf }
             }
 
             $itemObj = [PSCustomObject]@{
                 IsFolder = $item[-1] -ne '!'
-                Name     = Split-Path -Path $Path -Leaf
+                RawName  = $itemName
+                Name     = $itemName.TrimEnd('!')
                 # Item     = $item
                 Path     = $item
+                PathType = $PathType
+                MockFS   = $this
             }
         
+            $parseNameFn = {
+                param($Name)
+                $newPath = if ($this.Path) { Join-Path $this.Path $Name } else { $Name }
+                $this.MockFS.GetEntry($newPath)
+            }
+
             $itemObj | Add-Member -MemberType ScriptMethod -Name GetFolder -Value { return $this }
-            $itemObj | Add-Member -MemberType ScriptMethod -Name ParseName `
-                -Value {
-                    param($Name)
-                    $this.GetEntry("$Path/$Name", $PathType)
-                }
-            
+            $itemObj | Add-Member -MemberType ScriptMethod -Name ParseName -Value $parseNameFn
+
             return $itemObj
         } elseif ($CreateIfNotExists) {
             $this.AddItem($Path)
@@ -89,7 +149,7 @@ class MockFileSystem {
 
 # This is a representation of our mocked file system on the attached device.
 $DeviceFS = @(
-    '/',
+    '', # Root folder
     'Internal storage',
     'Internal storage/MTPFilesTestDirA',
     'Internal storage/MTPFilesTestDirB',
@@ -106,160 +166,54 @@ $DeviceFS = @(
 
 # This represents the host filesystem, relative to the current directory.
 $HostFS = @(
-    'SomeFile!',
-    'SomeFile.txt!',
-    'SomeHostFolder',
-    'SomeHostFolder/HostSubFolderA',
-    'SomeHostFolder/HostSubFolderA/HostFileA!',
-    'SomeHostFolder/HostSubFolderA/HostFileA.txt!',
-    'SomeHostFolder/HostFileB!',
-    'SomeHostFolder/HostFileB.txt!',
-    'Internal storage', # Same name as device top-level folder
-    'Internal storage/HostSubFolderB',
-    'Internal storage/HostFileC.txt!',
+    '.',
+    './SomeFile!',
+    './SomeFile.txt!',
+    './SomeHostFolder',
+    './SomeHostFolder/HostSubFolderA',
+    './SomeHostFolder/HostSubFolderA/HostFileA!',
+    './SomeHostFolder/HostSubFolderA/HostFileA.txt!',
+    './SomeHostFolder/HostFileB!',
+    './SomeHostFolder/HostFileB.txt!',
+    './Internal storage', # Same name as device top-level folder
+    './Internal storage/HostSubFolderB',
+    './Internal storage/HostFileC.txt!',
     'C:',
-    'C:\HostFileD', # Folder with same name as file
-    'C:\HostFileD!',
-    'C:\HostFileD.txt!',
-    'C:\HostSubFolderC/HostFileE!',
-    'C:\HostSubFolderC/HostFileE.txt!'
+    'C:/HostFileD', # Folder with same name as file
+    'C:/HostFileD!',
+    'C:/HostFileD.txt!',
+    'C:/HostSubFolderC/HostFileE!',
+    'C:/HostSubFolderC/HostFileE.txt!'
 )
-
-$MockDeviceFS   = [MockFileSystem]::new('MockDeviceFS', $true, $DeviceFS)
-$MockHostFS     = [MockFileSystem]::new('MockHostFS', $false, $HostFS)
-
-
-# $MockDeviceFileSystem = @{
-#     'Internal storage' = @{
-#         'MTPFilesTestDirA' = @{ };
-#         'MTPFilesTestDirB' = @{
-#             'DeviceFileA' = "file";
-#             'DeviceFileA.txt' = 'file';
-#             'DeviceFileB.txt' = 'file';
-#             'DeviceFileA.jpg' = 'file';
-#         };
-#         'MTPFilesTestDirC' = @{ };
-#         'MTPFilesTestFile' = 'file';
-#         'MTPFilesTestFile.doc' = 'file';
-#     }
-# }
-
-# $MockHostFileSystem = @{
-    #     'SomeFile' = 'file';
-    #     'SomeFile.txt' = 'file';
-    #     'SomeHostFolder' = @{
-        #         'HostSubFolderA' = @{
-#             'HostFileA' = 'file';
-#             'HostFileA.txt' = 'file';
-#         };
-#         'HostFileB' = 'file';
-#         'HostFileB.txt' = 'file';
-#     };
-#     # This simulates a host directory with the same name as the device's top-level folder.
-#     'Internal storage' = @{
-#         'HostSubFolderB' = @{ };
-#         'HostFileC.txt' = 'file';
-#     };
-#     'C:' = @{
-#         'HostFileD' = 'file';
-#         'HostFileD.txt' = 'file';
-#         'HostSubFolderC' = @{
-#             'HostFileE' = 'file';
-#             'HostFileE.txt' = 'file';
-#         };
-#     };
-# }
-
-# function Get-MockedItem {
-#     param(
-#         [Parameter(Mandatory = $true)]
-#         [string]$Path,
-
-#         [scriptblock]$Function,
-
-#         [string[]]$FileSystem,
-
-#         [switch]$CreateIfNotExists
-#     )
-
-#     if ($FileSystem.Contains($Path, [StringComparison]::OrdinalIgnoreCase)) {
-        
-#     }
-
-#     foreach ($section in $pathSections) {
-#         # Navigate deeper down the path if the next section exists.
-#         if ($currentItem -is [hashtable] -and $currentItem.ContainsKey($section)) {
-#             $currentItem = $currentItem[$section]
-#         } else {
-#             # The path doesn't exist.
-#             return $null
-#         }
-#     }
-
-#     $itemObj = [PSCustomObject]@{
-#         IsFolder = $currentItem -is [hashtable]
-#         Name     = if ($pathSections) { $pathSections[-1] } else { "/" }
-#         Item     = $currentItem
-#         Path     = $Path
-#     }
-
-#     $itemObj | Add-Member -MemberType ScriptMethod -Name GetFolder -Value { return $this }
-#     $itemObj | Add-Member -MemberType ScriptMethod -Name ParseName `
-#         -Value (Set-ParseNameMethod -Path $Path -Function ${function:Get-MockedItem} -FileSystem $FileSystem)
-    
-#     #Write-Host "Path: $($itemObj.Path)"
-#     return $itemObj
-# }
-
-# function Set-ParseNameMethod {
-#     param(
-#         [Parameter(Mandatory = $true)]
-#         [string]$Path,
-
-#         [Parameter(Mandatory = $true)]
-#         [scriptblock]$Function,
-
-#         [string[]]$FileSystem
-#     )
-
-#     return {
-#         param($Name)
-#         & $Function -Path "$Path/$Name" -FileSystem $FileSystem
-#     }.GetNewClosure()
-# }
 
 # Capture the script location because Pester runs in a different scope.
 $BasePath = Split-Path $PSScriptRoot -Parent
+
+$MockDeviceFS   = [MockFileSystem]::new('MockDeviceFS', $false, $DeviceFS, $BasePath)
+$MockHostFS     = [MockFileSystem]::new('MockHostFS', $true, $HostFS, $BasePath)
 
 # Test a path against our mock host filesystem instead of the host itself.
 Mock Test-Path {
     param([string]$Path, [string]$PathType)
 
-    # Remove the script root from the path, turning it into a relative path.
-    $Path = $Path.Replace($BasePath, '')
-    if ($Path -eq '' -and $PathType -ne 'Leaf') {
-        # The path is the base path, so it's a valid container path.
-        return $true
-    }
-
     return $MockHostFS.Contains($Path, $PathType)
 }
 
 Mock Get-TargetDevice {
-    return $MockDeviceFS.GetEntry('/')
+    return $MockDeviceFS.GetEntry('')
 }
 
-Mock Get-IsDevicePath -RemoveParameterType 'Device' {
-    if ($null -eq $Device) {
-        return $false
-    }
+# Mock Get-IsDevicePath -RemoveParameterType 'Device' {
+#     if ($null -eq $Device) {
+#         return $false
+#     }
 
-    # Get the first segment of the path.
-    $topLevelPath = $Path.Replace('\', '/').Split('/', [StringSplitOptions]::RemoveEmptyEntries)[0]
+#     # Get the first segment of the path.
+#     $topLevelPath = $Path.Replace('\', '/').Split('/', [StringSplitOptions]::RemoveEmptyEntries)[0]
 
-    # Is it one of the mocked top-level folders?
-    return $MockDeviceFS.Contains($topLevelPath, 'Container')
-}
+#     # Is it one of the mocked top-level folders?
+#     return $MockDeviceFS.Contains($topLevelPath, 'Container')
+# }
 
 Mock Get-MTPIterator -RemoveParameterType 'ParentFolder' {
     $sections = $Path.Replace('\', '/').Split('/', [StringSplitOptions]::RemoveEmptyEntries)
@@ -293,9 +247,9 @@ Mock Get-MTPIterator -RemoveParameterType 'ParentFolder' {
 }
 
 Mock Get-COMFolder {
-    $isOnHost = Test-IsHostDirectory -DirectoryPath $Path
-    $fileSystem = if ($isOnHost) { $HostFS } else { $DeviceFS }
-    $item = Get-MockedItem -Path $Path -FileSystem $fileSystem -CreateIfNotExists $CreateIfNotExists
+    $isOnHost = (Get-PathType -Path $Path) -eq [PathType]::Host
+    $fileSystem = if ($isOnHost) { $MockHostFS } else { $MockDeviceFS }
+    $item = $fileSystem.GetEntry($Path, 'Container', $CreateIfNotExists)
     if ($item -and $item.IsFolder) {
         return $item
     }
